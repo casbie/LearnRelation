38b0cc873de813ae8cb2e0a97f97e75618f1a7a9
Yu-Ju Chen <casbie23@gmail.com>
2015-03-04 15:41:21 +0800

    refine information

--- ProcData/dep_parser.py
+++ ProcData/dep_parser.py
@@ -1,6 +1,17 @@
+#-*- encoding: utf-8
+
+##########################################################
+# * Given json file and dependency tree result,          #
+#   combine the feature of dependency tree to the result #
+# * Thanks Mr. Aahin and Mr. KC                          #
+# * Author: Yu-Ju Chen                                   #
+# * Date: 2014-12-26                                     #
+# * Modified: 2015-03-03                                 #
+##########################################################
+
 from pprint import pprint
 import re
-
+import uniout
 
 def parse(data):
 
@@ -86,13 +97,27 @@ def parse(data):
 
 
 def main():
-    data = ''
-    with open('input.txt') as f:
-        # get rid of \n and redundant whitespace
-        data = f.read()
-    print(data)
-    tree = parse(data)
-    pprint(tree)
+
+    f_json = open('../Data/sa_json/output.jsons')
+    
+    for i in range(1, 2):
+        
+        data_json = f_json.readline()
+
+        if i < 10:
+            filename = '000' + str(i)
+        elif i < 100:
+            filename = '00' + str(i)
+        elif i < 1000:
+            filename = '0' + str(i)
+        else:
+            filename = str(i)
+
+        f_dep = open('../Data/sa_parsing_t/'+filename)
+        data_dep = f_dep.read()
+        #print(data_dep)
+        tree = parse(data_dep)
+        pprint(tree)
 
 
 if __name__ == '__main__':
